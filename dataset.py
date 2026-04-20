b29f9e501f57ed103f08e162290d1f4843954c3c
Clóvis A Pelosini <capelosini@gmail.com>
2026-04-20 16:56:40 -0300

    Normalized Aspect Ratio of Dataset

--- dataset.py
+++ dataset.py
@@ -1,7 +1,32 @@
+from pathlib import Path
+
 import keras
+from PIL import Image
 
 image_size = (50, 50)
 batch_size = 128
+
+datasetFolder = Path("dataset/")
+
+
+def normalizeImagesRatio():
+    i = 0
+    print("Normalizing images ratio...")
+    for folder in datasetFolder.iterdir():
+        for imagePath in folder.iterdir():
+            img = Image.open(imagePath)
+            img = img.crop(
+                box=(
+                    (img.width / 2) - (img.height / 2),
+                    0,
+                    (img.width / 2) + (img.height / 2),
+                    img.height,
+                )
+            )
+            img.save(imagePath)
+            print(f"Normalized image {i}", end="\r")
+            i += 1
+
 
 train_ds, val_ds = keras.utils.image_dataset_from_directory(
     "dataset",
@@ -11,3 +36,6 @@ train_ds, val_ds = keras.utils.image_dataset_from_directory(
     image_size=image_size,
     batch_size=batch_size,
 )
+
+
+# normalizeImagesRatio()
